0814c6d8cce4f826991ff28dbbed77f38ff01f08
rezaluthfi <rezaluthfiakbar7@gmail.com>
2026-07-20 07:28:38 +0700

    fix: change fonts to Poppins to all screens

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -3,10 +3,26 @@ import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData get lightTheme {
+    final String? poppinsFont = GoogleFonts.poppins().fontFamily;
+
     return ThemeData(
       useMaterial3: false,
       scaffoldBackgroundColor: Colors.white,
+
+      fontFamily: poppinsFont,
+
       textTheme: GoogleFonts.poppinsTextTheme(ThemeData.light().textTheme),
+
+      appBarTheme: AppBarTheme(
+        backgroundColor: Colors.white,
+        elevation: 0.5,
+        titleTextStyle: TextStyle(
+          fontFamily: poppinsFont,
+          color: const Color(0xFF1A202C),
+          fontWeight: FontWeight.w600,
+          fontSize: 18,
+        ),
+      ),
     );
   }
 }
